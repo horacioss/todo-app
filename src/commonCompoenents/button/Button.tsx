89031e838677c4b68eb354b74a064cbd5d9622eb
Horacio Macias <horacioss2013@gmail.com>
2021-08-27 14:00:13 -0500

    Added someFunc

--- src/commonCompoenents/button/Button.tsx
+++ src/commonCompoenents/button/Button.tsx
@@ -1,5 +1,7 @@
 import styles from "./Button.module.scss";
 
+const somefunc = () => console.log("Hello");
+
 export const Button = (title: string) => {
   return <button className={styles.mybutton}>{title}</button>;
 };
